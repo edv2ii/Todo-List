b419dcec3837df240489c1e9f0b96473e0a5d13c
axyratio <kittiphong.u-.66@ubu.ac.th>
2024-05-13 22:21:04 +0700

    update

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
